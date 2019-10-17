b4e09d964c70518f237639d8d745b5a9b547411c
samSterns <sam.g.sterns@gmail.com>
2019-10-16 17:43:03 -0700

    html and meta data done

---
+++ common/load-profile.js
@@ -0,0 +1,22 @@
+import { getUser } from '../data/api.js';
+import gameOver from './game-over.js';
+function loadProfile() {
+    const name = document.getElementById('name');
+    const avatar = document.getElementById('avatar');
+    const wp = document.getElementById('wp');
+    const sc = document.getElementById('sc');
+    const user = getUser();
+    if (!user) {
+        window.location = './index.html';
+    }
+    name.textContent = user.name;
+    avatar.src = '../assets/avatars/' + user.race + '.png';
+    sc.textContent = user.sc;
+    if (gameOver(user)) {
+        wp.textContent = 'Better Try Again, my friend';
+    }
+    else {
+        wp.textContent = user.wp;
+    }
+}
+export default loadProfile; 
