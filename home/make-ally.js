03b0690f5cf32c087617df33ba29b0de2699b5ab
samSterns <sam.g.sterns@gmail.com>
2019-10-17 11:33:25 -0700

    compiling

--- home/make-ally.js
+++ home/make-ally.js
@@ -1,5 +1,10 @@
-function makeAlly(formData) {
-    const user = {
-        name: 
-    }
-}
+export const makeAlly = (formData) => {
+    const ally = {
+        name: formData.get('name'),
+        animal: formData.get('animal'),
+        wp: 35,
+        sc: 20,
+        completed: {}
+    };
+    return ally;
+};
